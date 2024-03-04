2b214f7bbb77420066203fee9feeceda6dbeb6fa
Emilse Arias <emilsearias@gmail.com>
2024-03-04 10:26:02 -0300

    agregamos los comandos start y dev para ejecutar el repo de backend en produccion y en desarrollo

---
+++ index.js
@@ -0,0 +1,5 @@
+import express from 'express';
+console.log('Hola mundo v2 dfjghdfjgh')
+
+//1- configurar un puerto
+// puedo compilar el index.js usando node --watch index.js en forma experimental
