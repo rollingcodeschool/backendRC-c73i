e55c9c6bc8407f1f63a2c09d4e00906182078001
Emilse Arias <emilsearias@gmail.com>
2024-03-04 10:49:05 -0300

    agregamos el puerto 4000 para el backend

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.log('Hola mundo v2 dfjghdfjgh')
 
 //1- configurar un puerto
 // puedo compilar el index.js usando node --watch index.js en forma experimental
+const app = express();
+//crear una variable
+app.set('port', 4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'))
+})
+
+//2 - configurar los middlewares del proyecto
+
+//3 - configurar las rutas
