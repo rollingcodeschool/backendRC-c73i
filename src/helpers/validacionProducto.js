df94c93f19bc1622b7ab84549c6d90ac69d78796
Emilse Arias <emilsearias@gmail.com>
2024-03-20 11:35:19 -0300

    agregamos las validaciones de descripciones, imagen y categoria de productos

--- src/helpers/validacionProducto.js
+++ src/helpers/validacionProducto.js
@@ -1,28 +1,55 @@
 import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 
-
 const validacionesProducto = [
-    check("nombreProducto")
-      .notEmpty()
-      .withMessage("El nombre del producto es un dato obligatorio")
-      .isLength({ min: 2, max: 50 })
-      .withMessage(
-        "El nombre del producto debe tener entre 2 y 50 caracteres"
-      ),
-    check("precio")
-      .notEmpty()
-      .withMessage("El precio es un dato obligatorio")
-      .isNumeric()
-      .withMessage("El precio debe ser un número")
-      .custom((value)=>{
-          if(value >= 50 && value<= 10000){
-              return true;
-          }else{
-              throw new Error("El precio debe estar entre $50 y $10000");
-          }
-      }),
-      (req, res, next)=>{resultadoValidacion(req,res,next)}
-  ]
+  check("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es un dato obligatorio")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres"),
+  check("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((value) => {
+      if (value >= 50 && value <= 10000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre $50 y $10000");
+      }
+    }),
+  check("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+    .withMessage(
+      "La imagen debe tener un formato de URL valida y terminar en jpg|jpeg|gif|png"
+    ),
+  check("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn(["Infusiones", "Batidos", "Dulce", "Salado"])
+    .withMessage(
+      "La categoria debe ser una de las siguientes opciones: 'Infusiones','Batidos', 'Dulce', 'Salado' "
+    ),
+    check("descripcion_breve")
+    .notEmpty()
+    .withMessage("La descripcion breve es un dato obligatorio")
+    .isLength({ min: 5, max: 50 })
+    .withMessage(
+      "La descripcion debe tener entre 5 y 50 caracteres"
+    ),
+    check("descripcion_amplia")
+    .notEmpty()
+    .withMessage("La descripcion amplia es un dato obligatorio")
+    .isLength({ min: 50, max: 300 })
+    .withMessage(
+      "La descripcion amplia debe tener entre 50 y 300 caracteres"
+    ),
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
+];
 
-  export default validacionesProducto;
+export default validacionesProducto;
